f21dd9aea399b5da6744cf1067b9abae866dbd6e
Brandon Piña <brandonpi265@gmail.com>
2022-11-16 01:33:01 +0000

    Use devcontainer because windows is big stupid

--- build.rs
+++ build.rs
@@ -2,12 +2,13 @@ extern crate meson;
 use std::env;
 use std::path::PathBuf;
 
-fn main(){
+fn main() {
+    env::set_var("RUST_BACKTRACE", "1");
     let build_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    build_path.join("build");
+    _ = build_path.join("build");
     let build_path = build_path.to_str().unwrap();
 
     println!("cargo:rustc-link-lib=libvmaf");
     println!("cargo:rustc-link-search=native={build_path}");
-    meson::build("vmaf/libvmaf",build_path);
+    meson::build("vmaf/libvmaf", build_path);
 }
