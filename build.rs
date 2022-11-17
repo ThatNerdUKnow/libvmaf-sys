0d38ca6fc23f99ff36701768f65e38aa40e21398
Brandon Piña <brandonpi265@gmail.com>
2022-11-16 21:05:23 -0600

    update build.rs

--- build.rs
+++ build.rs
@@ -5,25 +5,31 @@ use std::path::PathBuf;
 
 fn main() {
     //env::set_var("RUST_BACKTRACE", "1");
-    let build_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    _ = build_path.join("build");
-    let build_path = build_path.to_str().unwrap();
+    let build_dir = PathBuf::from(env::var("OUT_DIR").unwrap()).join("build");
+    let lib_dir = build_dir.join("src");
 
-    println!("cargo:rustc-link-lib=libvmaf");
-    println!("cargo:rustc-link-search=native={build_path}");
-    meson::build("vmaf/libvmaf", build_path);
+    let build_dir_str = build_dir.to_str().unwrap();
+    let lib_dir_str = lib_dir.to_str().unwrap();
 
-    let include_path = PathBuf::from("vmaf/libvmaf/include");
-    let binding = canonicalize(include_path).unwrap();
-    let include_path = binding.to_str().unwrap();
-    //println!("cargo:include=vmaf/libvmaf/include");
+    meson::build("vmaf/libvmaf", build_dir_str);
+
+    println!("cargo:rustc-link-lib=static=libvmaf");
+    println!("cargo:rustc-link-search=native={lib_dir_str}");
+
+    // Path to vendor header files
+    let headers_dir = PathBuf::from("vmaf/libvmaf/include");
+    let headers_dir_canonical = canonicalize(headers_dir).unwrap();
+    let include_path = headers_dir_canonical.to_str().unwrap();
+
+    // Generate bindings to libvmaf using rust-bindgen
     let bindings = bindgen::Builder::default()
-    .header("vmaf/libvmaf/include/libvmaf/libvmaf.h")
-    .clang_arg(format!("-I{include_path}"))
-    .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-    .generate()
-    .expect("Unable to generate bindings");
+        .header("vmaf/libvmaf/include/libvmaf/libvmaf.h")
+        .clang_arg(format!("-I{include_path}"))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
 
+    // Write bindings to build directory
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
         .write_to_file(out_path.join("bindings.rs"))
