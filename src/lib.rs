711e82feac89479cb73074cd03ea5f187ffa4610
Brandon Piña <brandonpi265@gmail.com>
2022-12-31 22:38:37 -0600

    crate level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
-// Test
+//! This crate is a simple bindgen wrapper library around Netflix's `libvmaf`  
+//! You're probably looking for [libvmaf-rs](https://crates.io/crates/libvmaf-rs)
+
+
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
